43e3012744d7d8126973ed70efbe8a3ffc02179a
Gulzar Ahmed <alignwebs@gmail.com>
2022-09-01 01:00:10 +0530

    init

---
+++ precache-manifest.eb5c369e2d7d47647917cbe68da04187.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debf3a484d12355dd33417906c323f6b",
+    "url": "/.htaccess"
+  },
+  {
+    "revision": "d89e7c6bda158ffeb766dd0d70814c06",
+    "url": "/banner1.jpg"
+  },
+  {
+    "revision": "34ac1da4e876e41ffa88b7dff37509a9",
+    "url": "/banner2.jpg"
+  },
+  {
+    "revision": "5d3eb52e810a7d34b7a0",
+    "url": "/css/app.876487ef.css"
+  },
+  {
+    "revision": "8e3bf56afdf6a5e2b880",
+    "url": "/css/chunk-vendors.218a4b38.css"
+  },
+  {
+    "revision": "aa654ee829b6374eae0f0ddffcb5585a",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "d89e7c6bda158ffeb766dd0d70814c06",
+    "url": "/img/banner1.d89e7c6b.jpg"
+  },
+  {
+    "revision": "9c96e0ed7093c095fd33870329282962",
+    "url": "/img/flags.9c96e0ed.9c96e0ed.png"
+  },
+  {
+    "revision": "8d8f1ee0c11f58c9d68d48a4cf621339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3eb52e810a7d34b7a0",
+    "url": "/js/app.e1a723c8.js"
+  },
+  {
+    "revision": "8e3bf56afdf6a5e2b880",
+    "url": "/js/chunk-vendors.9688a1de.js"
+  },
+  {
+    "revision": "07f518c743563a7f721760ca3f8bb579",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "9b4026a6a084abc541359b39e0e66ec5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "be2d23bdaafe243c70cb59ad83d960bd",
+    "url": "/memebers.jpg"
+  },
+  {
+    "revision": "f90ea3538da0fc3667be5d672c4a9f75",
+    "url": "/nginx.config"
+  },
+  {
+    "revision": "0f3b5e0956ed4549d12c7f46affaed3c",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d14e089e96cedb7c712f6d33ee0308da",
+    "url": "/yefikermead.jpg"
+  }
+]);
